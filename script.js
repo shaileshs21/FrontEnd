4d0a6d7f91573a2eb107843c8a89ecd32a61d3eb
shailesh <shaileshwaghmare3@gmail.com>
2019-02-11 17:42:25 +0530

    minimize sidebar not working

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded",function(){
         }
     }
 })
-
+/* Collapsible Side Menus */
 var coll = document.getElementsByClassName("collapsible");
 var i;
 for(i = 0;i < coll.length;i++){
@@ -24,7 +24,7 @@ function collapseAnimate(){
 
     coll[coll.length-1].nextElementSibling.style.maxHeight = null;
     console.log(this);
-
+    this.classList.toggle("open"); 
     var submenu = this.nextElementSibling;
     if(submenu.style.maxHeight){
         submenu.style.maxHeight = null;
@@ -46,3 +46,15 @@ function collapse(){
     }
 }
 */
+
+/*  Sidebar Minimize */
+
+function minimize(){
+    console.log("in Minimize sidebar");
+    var sidebar = document.getElementsByClassName("sidebar");
+    //sidebar.style.maxWidth = "0px";
+
+    console.log(sidebar);
+   // sidebar.classList.toggle("sidebarWidth");
+ 
+}
